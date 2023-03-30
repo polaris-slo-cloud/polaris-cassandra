3636f61ac373e6907537d739afd2184505866969
Nico Kratky <nico@nicokratky.me>
2023-03-30 16:25:28 +0200

    feat: generate horizontal elasticity strategy controller

---
+++ apps/horizontal-elasticity-strategy-controller/jest.config.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+export default {
+  displayName: 'horizontal-elasticity-strategy-controller',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory:
+    '../../coverage/apps/horizontal-elasticity-strategy-controller',
+};
