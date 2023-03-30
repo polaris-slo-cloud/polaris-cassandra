46be0e9880266cc49b35cf9a648a3d412c8f1887
Nico Kratky <nico@nicokratky.me>
2023-03-30 16:21:22 +0200

    feat: generate horizontal elasticity strategy

--- libs/elasticity-strategies/src/index.ts
+++ libs/elasticity-strategies/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/elasticity-strategies';
 export * from './lib/init-polaris-lib';
 export * from './lib/elasticity/horizontal-vertical-elasticity-strategy.prm';
+export * from './lib/elasticity/horizontal-elasticity-strategy.prm';
