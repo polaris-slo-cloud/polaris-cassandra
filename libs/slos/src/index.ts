c88174001a4234dd7fe85cd86af6ec19d42e6cbd
Nico Kratky <nico@nicokratky.me>
2023-04-03 17:48:57 +0200

    feat: generate efficiency slo mapping type

--- libs/slos/src/index.ts
+++ libs/slos/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/slos';
 export * from './lib/init-polaris-lib';
 export * from './lib/metrics/efficiency-metric.prm';
+export * from './lib/slo-mappings/efficiency.slo-mapping.prm';
