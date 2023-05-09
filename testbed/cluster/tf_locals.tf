35e306ddb5840c3476bcd50b07565276b711026e
Nico Kratky <nico@nicokratky.me>
2023-05-09 17:34:59 +0200

    feat: enable soft podAntiAffinity
    This allows multiple k8ssandra nodes to run on the same k8s node

--- testbed/cluster/tf_locals.tf
+++ testbed/cluster/tf_locals.tf
@@ -30,6 +30,16 @@ locals {
     storage                = 1
     soft_pod_anti_affinity = true
     enable_prometheus      = true
-    stargate_size          = 1
+    stargate_size          = 0
+    resources = {
+      requests = {
+        cpu    = 1
+        memory = "1G"
+      }
+      limits = {
+        cpu    = 2
+        memory = "4G"
+      }
+    }
   }
 }
