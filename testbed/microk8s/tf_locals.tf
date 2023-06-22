e02c4657bda5f4970fff295c6afc85cf7b1a48fc
Nico Kratky <nico@nicokratky.me>
2023-06-22 18:56:24 +0200

    refactor: increase storage

--- testbed/microk8s/tf_locals.tf
+++ testbed/microk8s/tf_locals.tf
@@ -6,7 +6,7 @@ locals {
   k8ssandra_config = {
     cluster_name           = "polaris-k8ssandra-cluster"
     cluster_size           = 1
-    storage                = 1
+    storage                = 3
     soft_pod_anti_affinity = true
     enable_prometheus      = true
     stargate_size          = 0
