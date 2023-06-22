373fa73163046fe0cbf51f8aa1f6039e0f6dc34c
Nico Kratky <nico@nicokratky.me>
2023-06-22 18:57:05 +0200

    refactor: update k8ssandra helm chart

--- testbed/microk8s/k8s_k8ssandra.tf
+++ testbed/microk8s/k8s_k8ssandra.tf
@@ -1,7 +1,8 @@
 resource "helm_release" "k8ssandra_operator" {
-  name      = "k8ssandra-operator"
-  chart     = "https://helm.k8ssandra.io/stable/k8ssandra-operator-1.6.0.tgz"
-  namespace = lookup(kubernetes_namespace.this, "k8ssandra")["id"]
+  name       = "k8ssandra-operator"
+  repository = "https://helm.k8ssandra.io/stable"
+  chart      = "k8ssandra-operator"
+  namespace  = lookup(kubernetes_namespace.this, "k8ssandra")["id"]
 }
 
 resource "kubectl_manifest" "k8ssandra_cluster" {
