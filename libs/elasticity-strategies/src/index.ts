2b35c522e4d31b8cd76dad4ba42852a909a9af69
Nico Kratky <nico@nicokratky.me>
2023-09-06 11:59:29 +0200

    feat: abstract K8ssandraCluster CRD

--- libs/elasticity-strategies/src/index.ts
+++ libs/elasticity-strategies/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/elasticity-strategies';
 export * from './lib/init-polaris-lib';
 export * from './lib/elasticity/k8ssandra-elasticity-strategy.prm';
 export * from './lib/k8ssandra-cluster.prm';
+export * from './lib/elasticity/k8ssandra-elasticity-strategy-controller.base';
--- libs/elasticity-strategies/src/lib/k8ssandra-cluster.prm.ts
+++ libs/elasticity-strategies/src/lib/k8ssandra-cluster.prm.ts
@@ -5,15 +5,38 @@ export class K8ssandraClusterSpec {
 }
 
 export class CassandraSpec {
+  clusterName: string;
+
   datacenters: [{
-    size: number,
+    /**
+     * Size is the number Cassandra pods to deploy in this datacenter.
+     */
+    size: number;
+
+    /**
+     * Stopped means that the datacenter will be stopped. Use this for maintenance or for cost saving.
+     * A stopped CassandraDatacenter will have no running server pods, like using "stop" with traditional System V init
+     * scripts. Other Kubernetes resources will be left intact, and volumes will re-attach when the CassandraDatacenter
+     *  workload is resumed.
+     */
+    stopped: boolean;
   }];
 
+  /**
+   * Resources is the cpu and memory resources for the cassandra container.
+   */
   resources: {
+    /**
+     * Requests describes the minimum amount of compute resources required. If Requests is omitted for a container, it
+     * defaults to Limits if that is explicitly specified, otherwise to an implementation-defined value.
+     */
     requests?: {
       cpu: string,
       memory: string,
     },
+    /**
+     * Limits describes the maximum amount of compute resources allowed.
+     */
     limits?: {
       cpu: string,
       memory: string,
